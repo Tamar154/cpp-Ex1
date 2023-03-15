8762c37beda598040ad997748d70a9c318fb045e
Tamar154 <tamar.d154@gmail.com>
2023-03-15 19:25:22 +0200

    Filling the functions

--- AdptArray.c
+++ AdptArray.c
@@ -1,7 +1,6 @@
 #include "AdptArray.h"
 #include <stdlib.h>
 
-
 struct AdptArray_
 {
     PElement *arr;
@@ -11,39 +10,77 @@ struct AdptArray_
     DEL_FUNC del_func;
     COPY_FUNC copy_func;
     PRINT_FUNC print_func;
-} adpt;
+};
 
 /*
-    Initialize an empty array. 
+    Initialize an empty array.
 */
 PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC delete, PRINT_FUNC print)
 {
-    adpt.size = 0;
-    adpt.capacity = 1;
+    PAdptArray adpt = (PAdptArray)malloc(sizeof(PAdptArray));
 
-    adpt.arr = malloc (adpt.capacity * sizeof(PElement));
-    
-    adpt.copy_func = copy;
-    adpt.del_func = delete;
-    adpt.print_func = print;
+    adpt->arr = NULL;
+    adpt->size = 0;
+    adpt->capacity = 0;
+
+    adpt->copy_func = copy;
+    adpt->del_func = delete;
+    adpt->print_func = print;
+
+    return adpt;
 }
 
-void DeleteAdptArray(PAdptArray adpt)
+void DeleteAdptArray(PAdptArray arr)
 {
+    for (int i = 0; i < arr->capacity; i++)
+    {
+        free(arr->arr[i]);
+    }
 
+    free(arr);
 }
 
 Result SetAdptArrayAt(PAdptArray adpt, int index, PElement element)
 {
+    if (index >= adpt->capacity) 
+    {
+        adpt = realloc(adpt, sizeof(int)*index);
 
+        if (adpt == NULL)
+            return FAIL;
+
+        adpt->capacity = index+1; 
+    }
+    
+    if (adpt->arr[index] != NULL)
+    {
+        free(adpt->arr[index]);
+    }
+    adpt->arr[index] = adpt->copy_func(element);
+    adpt->size++;
+
+    return SUCCESS;
+}
+
+PElement GetAdptArrayAt(PAdptArray adpt, int index)
+{
+    if (index <= adpt->capacity && adpt->arr[index] != NULL)
+    {
+        return adpt->copy_func(adpt->arr[index]);
+    }
+
+    return NULL;
 }
 
 int GetAdptArraySize(PAdptArray adpt)
 {
-
+    return adpt->size;
 }
 
-void PrintDB(PAdptArray)
+void PrintDB(PAdptArray adpt)
 {
-
+    for(int i = 0; i <= adpt->size; i++)
+    {
+        adpt->print_func(adpt->arr[i]);
+    }
 }
